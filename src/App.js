b3ff5832c349e8750d2bbc5bb58b3a579dee22cb
Sean Lu <Sean.Lu@jetblue.com>
2020-05-14 15:01:16 -0400

    added develop branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-
 function App() {
   return (
     <div className="App">
